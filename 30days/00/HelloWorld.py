28fa562f24239a08d3df63d23451ad8accbc4462
NahLima <naiarafx@gmail.com>
2022-04-20 20:33:48 -0300

    acrescenta ola mundo!

--- 30days/00/HelloWorld.py
+++ 30days/00/HelloWorld.py
@@ -3,6 +3,6 @@ input_string = input()
 
 # Print a string literal saying "Hello, World." to stdout.
 print('Hello, World.')
-
+print("Olá Mundo")
 # TODO: Write a line of code here that prints the contents of input_string to stdout.
 print(str(input_string))
